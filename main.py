249e8bfd484f280d11cc680d277559f05bf400e2
KirillGorilo <kirill.007.228.74@gmail.com>
2022-12-07 17:31:38 +0900

    добавлено перемещение стрелок, добавлено падение объекта

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ colors_lists = {
     "GREEN": (0, 255, 0),
 }
 
-sc = pygame.display.set_mode((700, 900))
+W = 600
+H = 900
+
+sc = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Игра - башенки")
 # pygame.display.set_icon(pygame.image.load("icon.bmp")) # иконка игры
 
 FPS = 60
 clock = pygame.time.Clock()
 
-x = 350
+x = 300
 y = 200
 speed = 5
 
@@ -29,7 +32,22 @@ while 1:
             exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                y += speed
+                space_key = True
+
+            # elif event.type == pygame.KEYUP:
+            #     if event.key in [pygame.K_SPACE]:
+            #         space_key = False
+
+    keys = pygame.key.get_pressed()
+
+    if space_key:
+        if y >= 720:
+            space_key = False
+        y += speed
+    elif keys[pygame.K_LEFT]:
+        x -= speed
+    elif keys[pygame.K_RIGHT]:
+        x += speed
 
     sc.fill(colors_lists.get("WHITE"))
     pygame.draw.rect(sc, colors_lists.get("BLUE"), (x, y, 20, 20))
