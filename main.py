9c1b8873326c4be1ec4433812ea6b27c31958332
LybitelMamok <rozanov79145984237@gmail.com>
2022-12-07 17:29:06 +0900

    минимальный каркас

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+import pygame
+
+pygame.display.set_mode((600, 400))
+
+flRunning = True
+while flRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            flRunning = False
+
+print ("программа продолжает работу")
+
